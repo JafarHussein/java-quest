ea6206bf85703c61f2c6aae5e89afc9f5b9c0b35
floppyburgers019 <sanjaygreg19@gmail.com>
2026-09-01 09:27:52 +0300

    Arithemetic operators in java

---
+++ Arithemetic.java
@@ -0,0 +1,48 @@
+public class Arithemetic {
+    public static void main(String[] args){
+
+        //Arithemetic operators in java
+
+        int x = 10;
+
+        int y = 2;
+
+        int z;
+
+
+        /*
+        
+        1.Addition - z = x + y;
+
+        2.Subtraction - z= x - y;
+
+        3.Multiplication- z = x * y;
+
+        4.Division - z = x / y;
+
+        5.Modulus - z = x % y ;
+
+        Augemented Assignment
+
+        6.x += y
+
+        7. x -= y
+
+        8. x *= y
+
+        10. x /= y- Integer division is going to return only whole numbers
+
+        11. x %= y
+
+        Increment and decrement operators
+
+        12. x++;-This basically means x + 1
+
+        13. x-- - This basically means x - 1
+
+        14.
+        
+
+        */
+    }
+}
