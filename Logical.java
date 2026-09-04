4461502a281432e21f08a323e7a0b0144ddbbe8a
floppyburgers019 <sanjaygreg19@gmail.com>
2026-09-04 11:24:53 +0300

    Logical operators in java

---
+++ Logical.java
@@ -0,0 +1,13 @@
+public class Logical {
+
+    public static void main(String[] args){
+
+        // Logical operators , they allow us to check or modify more than one condition
+        // &&-AND this checks that both conditions are true
+        // ||-OR this checks to ensure at least one condition is true
+        // !-NOT this checks to reverses the conditions
+
+
+    }
+    
+}
