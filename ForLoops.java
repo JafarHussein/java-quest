4990c22ec56c41f61a6a73486473ed1d5676d40e
floppyburgers019 <sanjaygreg19@gmail.com>
2026-09-05 22:52:28 +0300

    For loops in java

---
+++ ForLoops.java
@@ -0,0 +1,15 @@
+public class ForLoops{
+
+    public static void main(String[] args){
+
+        // For loops in java-For loops execute for a finite number of times
+
+        for(int i =0; i <=10; i++){
+
+            System.out.println(i);
+
+        }
+
+
+    }
+}
