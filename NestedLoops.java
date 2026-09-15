b7c73047ba3e04bc3b16d813c3f609d8aed4f7b4
floppyburgers019 <sanjaygreg19@gmail.com>
2026-09-15 15:20:52 +0300

    nested loops in java

---
+++ NestedLoops.java
@@ -0,0 +1,16 @@
+public class NestedLoops {
+
+    public static void main(String[] args){
+
+        for(int i=0; i<3; i++){
+
+            for(int j=0; j<11; j++){
+
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+    
+}
